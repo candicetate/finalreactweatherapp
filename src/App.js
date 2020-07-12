96c2a7064ce85fc43b1760fcae29990e258a1483
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-07-11 20:02:26 -0600

    Added CSS files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "./Container";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Container />
+      <p class="footer">
+        <a href="https://github.com/candicetate/finalreactweatherapp">
+          Open Source
+        </a>{" "}
+        code by{" "}
+        <a href="https://www.linkedin.com/in/candice-tate-8a735246/">
+          Candice Tate
         </a>
-      </header>
+      </p>
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Container.css";
+
+export default function Container() {
+  return <h1>Hello</h1>;
+}
